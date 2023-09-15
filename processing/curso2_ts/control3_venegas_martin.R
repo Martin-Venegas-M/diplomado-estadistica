81dfaea9f76fd5ef9678f03aef59dfe658d4f787
mvenegasm <mvenegasm@ine.gob.cl>
2023-09-15 15:45:03 -0300

    update prueba

--- processing/curso2_ts/control3_venegas_martin.R
+++ processing/curso2_ts/control3_venegas_martin.R
@@ -15,7 +15,8 @@ pacman::p_load(tidyverse,
                summarytools,
                forecast,
                MASS,
-               uroot
+               uroot,
+               lmtest
                
 )
 
@@ -34,6 +35,10 @@ df <- readxl::read_xlsx("input/data/original/IMACEC_PIB.xlsx") %>%
 # 3. Preparación --------------------------------------------------------------------------------------------------------------------------
 
 Y <- ts(df$imacec, start = c(df$annio[1],df$mes[1]), end = c(2022,12), frequency = 12) # Crear objeto TS
+
+### Por si acaso...
+
+df$tiempo <- df$annio + (df$mes -1)/12 # Agregar variable de tiempo a la ase
 
 # 4. Realización del taller ---------------------------------------------------------------------------------------------------------------------------------
 
@@ -80,8 +85,12 @@ TS.diag(Y)
 
 # (c) Utilizando forecast::BoxCox.lambda(...) y forecast::BoxCox(...) evalúe una transformación. ----
 
-forecast::BoxCox.lambda(Y, method = "loglik") # Da un lambda de 1.2
+lambda <- forecast::BoxCox.lambda(Y, method = "loglik") # Da un lambda de 1.2
 Y_v <- forecast::BoxCox(Y, lambda = 1.2) # Da un lambda de 1.2
+
+# Por si acaso...
+
+df$imacec_v <- (df$imacec ^ lambda - 1) / lambda # Crear ima cec transformado con BoxCox en la base de datos.
 
 ## Revisar plots
 plot.ts(
@@ -96,7 +105,12 @@ plot.ts(
 shapiro.test(Y) # No distribuye normalmente
 shapiro.test(Y_v) # No distribuye normalmente
 
-# COMENTARIO: A priori, la transformación no parece hacer mucho.
+## Test de Breusch- Pagan
+bptest(imacec ~ tiempo, data = df)
+bptest(imacec_v ~ tiempo, data = df)
+
+
+# COMENTARIO: A priori, la transformación no parece hacer mucho. Se confirma con el test de Breusch-Pagan.
 
 # d) Utilizando forecast::ndiffs(...) y forecast::nsdiffs(...) evalúe los grados de integración de la serie. ----
 
@@ -190,6 +204,8 @@ TS.diag(m_auto$residuals) # Ver diagnosticos
 # Veamos ACF y PACF COMPARADOS
 par(mfrow = c(1, 2))
 acf(m_auto$residuals, ylim = c(-0.25, 1), lwd = 2); pacf(m_auto$residuals, lwd = 2)
+Box.Ljung.Test(m_auto$residuals)
+
 shapiro.test(m_auto$residuals)
 
 plot.ts(m_auto$residuals)
@@ -200,16 +216,96 @@ plot.ts(m_auto$residuals)
 
 # (g) Proponga una mejora al modelo propuesto en (e) utilizando el argumento fixed en la función forecast::Arima(...).  ----
 
-f <- c(
-  rep(NA, 6), 0, rep(NA, 2), 0, rep(NA, 3), 0, # Fijar valores 7, 10 y 14 para los parámetros φ (AR) y θ (MA)
-  rep(NA, 5), 0, rep(NA, 2), 0, NA, 0, NA, 0, rep(NA, 6), 0, # Fijar valores 6, 9 , 11, 13 y 20 para los parámetros φ (AR) y θ (MA)
-  
-  rep(NA, 6), 0, rep(NA, 2), 0, rep(NA, 3), 0, # Fijar valores 7, 10 y 14 para los parámetros estacionales Φ (SAR) y Θ (SMA)
-  rep(NA, 5), 0, rep(NA, 2), 0, NA, 0, NA, 0, rep(NA, 6), 0 # Fijar valores 6, 9 , 11, 13 y 20 para los parámetros estacionales Φ (SAR) y Θ (SMA)
-  )
+# f <- c(
+#   rep(NA, 6), 0, rep(NA, 2), 0, rep(NA, 3), 0, # Fijar valores 7, 10 y 14 para los parámetros φ (AR) y θ (MA)
+#   rep(NA, 5), 0, rep(NA, 2), 0, NA, 0, NA, 0, rep(NA, 6), 0, # Fijar valores 6, 9 , 11, 13 y 20 para los parámetros φ (AR) y θ (MA)
+#   
+#   rep(NA, 6), 0, rep(NA, 2), 0, rep(NA, 3), 0, # Fijar valores 7, 10 y 14 para los parámetros estacionales Φ (SAR) y Θ (SMA)
+#   rep(NA, 5), 0, rep(NA, 2), 0, NA, 0, NA, 0, rep(NA, 6), 0 # Fijar valores 6, 9 , 11, 13 y 20 para los parámetros estacionales Φ (SAR) y Θ (SMA)
+#   )
+
+### Crear vectores para fijar parametros
+AR <- c(NA)
+MA <- c(rep(0, 5), NA)
+SAR <- c(NA)
+SMA <- c(rep(0, 5), NA)
 
 m_fixed <- forecast::Arima(Y,
-  order = c(14, 0, 20),
-  seasonal = c(14, 1, 20),
-  fixed = f
-  )
+  order = c(1, 0, 6),
+  seasonal = c(1, 1, 6),
+  fixed = c(AR, MA, SAR, SMA)
+  ) ### Ajustar modelo fijado 1 
+
+m_fixed # Ver modelo
+salida.arima(Y, m_fixed) # Ver salida modelo
+TS.diag(m_fixed$residuals) # Ver diagnostico modelo
+
+par(mfrow = c(1, 2))
+acf(m_fixed$residuals, ylim = c(-0.25, 1), lwd = 2); pacf(m_fixed$residuals, lwd = 2)
+Box.Ljung.Test(m_fixed$residuals)
+
+plot.ts(m_fixed$residuals)
+
+# (h) Con forecast::forecast(...) genere pronósticos hasta diciembre de 2027.  ----
+
+annio_p <-2023:2027
+mes_p <-rep(1:12,4)
+tiempo_p <- annio_p + (mes_p - 1)/12
+tiempo_p <- tiempo_p[1:length(tiempo_p)] # Para empezar de Enero del 2023
+
+df_preds <- forecast::forecast(m_auto,
+                               h = length(tiempo_p), level = 0.95)
+
+### Generar tabla de predicciones
+P <- cbind(
+  df_preds$mean, 
+  df_preds$lower, 
+  df_preds$upper)
+
+### Plotear predicción
+par(bty = "n", las = 1, mfrow = c(1,1), font.main = 1)
+plot(df_preds, xlim = c(2000, 2030))
+lines(df_preds$fitted, col = "maroon")
+abline(h = mean(Y), col = "darkblue", lty = 2)
+
+# (i) Compare el desempeño de sus pronostico con respecto a los valores reales observados hasta junio de 2023. ----
+
+######### MANUAL ##########
+df_2023 <- cbind(
+  df %>% filter(tiempo >= 2023.000 & tiempo <= 2023.417),
+  imacec_preds = df_preds$mean[1:6],
+  date = lubridate::ym(paste0(df_2023$annio, "-", df_2023$mes))
+    )
+
+# plot(df_2023$imacec ~ df_2023$tiempo, type = "l", col = "darkblue", ylab = "IMACEC No Minero", xlab = "Tiempo")
+# lines(df_2023$imacec_preds ~ df_2023$tiempo, col = "maroon")
+
+
+Y_2023 <- ts(df_2023$imacec, start = c(df_2023$annio[1],df_2023$mes[1]), end = c(2023,6), frequency = 12)
+Y_pred2023 <- ts(df_2023$imacec_pred, start = c(df_2023$annio[1],df_2023$mes[1]), end = c(2023,6), frequency = 12)
+
+# plot(Y_2023, type = "l", col = "darkblue", ylab = "IMACEC No Minero", xlab = "Tiempo", xaxt = 'n')
+# lines(Y_pred2023, col = "maroon")
+# axis(1, at=1:6, labels = c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"))
+
+
+library(ggplot2)
+
+ggplot(df_2023, aes(x = lubridate::ymd(date))) +
+  geom_line(aes(y = imacec, color = "Real")) +
+  geom_line(aes(y = imacec_preds, color = "Predicción")) +
+  geom_point(aes(y = imacec, color = "Real")) +
+  geom_point(aes(y = imacec_preds, color = "Predicción")) +
+  geom_text(aes(y = imacec, label = round(imacec, 2)), size = 3, vjust = -0.5, hjust = -0.2, color = "darkblue") +
+  geom_text(aes(y = imacec_preds, label = round(imacec_preds, 2)), size = 3, vjust = -0.5, hjust = -0.2, color = "maroon") +
+  labs(
+    y = "IMACEC No Minero",
+    x = "Tiempo",
+    title = "Valores reales y predichos de IMACEC NM 2023"
+  ) + 
+  theme_classic() +
+  scale_color_manual(values = c("Real" = "darkblue", "Predicción" = "maroon")) +
+  guides(color = guide_legend(title = "")) +
+  theme(legend.position='bottom')
+  # lims(x = c(min(df_2023$date), lubridate::ymd("2023-06-01")))  # Ajusta los límites del eje X
+
